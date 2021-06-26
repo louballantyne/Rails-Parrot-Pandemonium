03ecf4498ca28773ada2bb3bc1b6c1a5fc1520f4
Louise Ballantyne <lousballantyne@icloud.com>
2021-06-26 11:52:13 +0100

    User can post an answer to a question

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,7 +1,22 @@
 class RepliesController < ApplicationController
   def create
+    reply_params
+    @reply = Reply.create(user_id: session[:user_id], message_id: @message, body: @body)
+
+    if @reply.save!
+      redirect_to "/messages/#{@message}", notice: "Answer posted"
+    else
+      redirect_to "/messages/#{@message}", notice: "Please try to post again"
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def reply_params
+    @message = params[:message_id]
+    @body = params[:reply][:body]
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :messages
-
+  has_many :replies
+  
   validates :username, uniqueness: true
   validates :email, uniqueness: true
   #validates :password_confirmation, presence: true
